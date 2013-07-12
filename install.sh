aeab57503740a84b05d086cf29ea1c5ed0fd8250
cloudsben <cloudsben@gmail.com>
2013-07-12 16:10:09 +0800

    Added install shell file

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+ln -s ~/.bash-git/profile ~/.profile
+ln -s ~/.bash-git/gitconfig ~/.gitconfig
+ln -s ~/.bash-git/gitignore_global ~/.gitignore_global
+ln -s ~/.bash-git/tmux.conf ~/.tmux.conf
+echo 'done'
